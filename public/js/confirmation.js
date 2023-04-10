326ab844edc948d442910dbe14969e1ad24aa501
R3-Rex <71946789+R3-Rex@users.noreply.github.com>
2023-04-10 09:25:24 -0600

    Work on the download/shop links

--- public/js/confirmation.js
+++ public/js/confirmation.js
@@ -1,9 +1,44 @@
+var rawConfirmation = ""
 void function (script) {
     //const { searchParams } = new URL(script.src);
     var fetchLocation = "./public/html/confirmation.html"
     fetch(fetchLocation).then(r => r.text()).then(content => {
-        script.outerHTML = content;
-        
+        rawConfirmation = content;
+        var masterContent = "<div id=\"confirmation-window\"></div>"
+        script.outerHTML = masterContent;
     });
 
 }(document.currentScript);
+
+
+var hasAnswer = false
+var answerValue = false
+async function AskForConfirmation(title, subtitle, more_title, more_body, yes_text, no_text) {
+    var modifiedConfirmation = rawConfirmation
+    modifiedConfirmation = modifiedConfirmation.replace("%title%", title)
+    modifiedConfirmation = modifiedConfirmation.replace("%subtitle%", subtitle)
+    modifiedConfirmation = modifiedConfirmation.replace("%more_title%", more_title)
+    modifiedConfirmation = modifiedConfirmation.replace("%more_body%", more_body)
+    modifiedConfirmation = modifiedConfirmation.replace("%yes%", yes_text)
+    modifiedConfirmation = modifiedConfirmation.replace("%no%", no_text)
+    document.getElementById("confirmation-window").innerHTML = modifiedConfirmation
+
+    hasAnswer = false
+
+    while(!hasAnswer){
+        await new Promise(r => setTimeout(r, 100));
+    }
+
+    document.getElementById("confirmation-window").innerHTML = ""
+
+    return answerValue
+}
+
+function ConfirmationYes(){
+    hasAnswer = true
+    answerValue = true
+}
+function ConfirmationNo(){
+    hasAnswer = true
+    answerValue = false
+}
